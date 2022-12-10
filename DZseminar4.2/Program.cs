9cae77747bad9496321b5763290885514122797c
Ilya Perederey <ilya_soldat@mail.ru>
2022-12-11 00:10:49 +0300

    DZ4.2

---
+++ DZseminar4.2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("введите число:");
+var n = Convert.ToInt32(Console.ReadLine());
+var sum = 0;
+    while(n > 0)
+    {
+    sum += n % 10;
+     n /= 10;
+    }
+Console.WriteLine(sum);
+
+    

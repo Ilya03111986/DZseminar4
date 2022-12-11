ff0ed6ef5b7c3f89cd205c44679222fc5df9b4aa
Ilya Perederey <ilya_soldat@mail.ru>
2022-12-11 23:44:42 +0300

    DZ4.3

---
+++ DZSeminar4.3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+
+int[] myArray = new int[8];
+Random rand = new Random();
+Console.Write ("[");
+for (int i = 0; i < myArray.Length; i++)
+{
+myArray[i] = rand.Next(99);
+Console.Write( myArray [i] +(i < myArray.Length -1? "," : "") ) ;
+}
+Console.Write("]");
+
+     
+
